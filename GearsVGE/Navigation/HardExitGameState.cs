131237915de48371436cd8c1ff996575de677b84
Jonathan Basniak <jon@spectrumbranch.com>
2011-02-21 22:54:23 -0500

    Fixed Issue #112. HardExitGameState.cs now a MenuUserControl.
    Signed-off-by: Jonathan Basniak <jon@spectrumbranch.com>

--- GearsVGE/Navigation/HardExitGameState.cs
+++ GearsVGE/Navigation/HardExitGameState.cs
@@ -2,31 +2,11 @@
 
 namespace Gears.Navigation
 {
-    public sealed class HardExitGameState : IMenuItem
+    public sealed class HardExitGameState : MenuUserControl
     {
-        private string menuText;
-        public string MenuText
-        {
-            get { return menuText; }
-            set
-            {
-                //15 chars or less to fit release 2 of menu implementation
-                if (value.Length <= 15)
-                {
-                    menuText = value;
-                }
-                else
-                {
-                    //do nothing currently
-                }
-            }
-        }
-        public HardExitGameState()
-        {
-            MenuText = "Exit Game";
-        }
+        public HardExitGameState() : base("Exit Game") { }
 
-        public void ThrowPushEvent()
+        public override void ThrowPushEvent()
         {
             ContentButler.GetGame().Exit();
         }
