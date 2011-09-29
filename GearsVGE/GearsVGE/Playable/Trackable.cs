2e34da9f459089451e9acfa23503304d389802e9
Steven Barbaro <Steven@.(none)>
2011-09-28 21:28:51 -0400

    changing filename

---
+++ GearsVGE/GearsVGE/Playable/Trackable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gears.Playable
+{
+    public class Trackable
+    {
+        public string getUUID()
+        {
+            return this.uuid_.ToString();
+        }
+
+        private Guid uuid_ = Guid.NewGuid();
+    }
+}
