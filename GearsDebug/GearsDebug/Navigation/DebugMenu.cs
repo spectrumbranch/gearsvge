68917d2c5ba9f3f766a597042df70afb489d76a1
unknown <Joseph@.(none)>
2011-12-24 22:46:37 -0500

    added joe's menu item

--- GearsDebug/GearsDebug/Navigation/DebugMenu.cs
+++ GearsDebug/GearsDebug/Navigation/DebugMenu.cs
@@ -11,16 +11,18 @@ namespace GearsDebug.Navigation
         //Personal testing menus below.
         private SpectrumTestMenu spectrum = new SpectrumTestMenu();
         private DevelopmentMenu unsorted = new DevelopmentMenu();
+        private JoeTest tatltael = new JoeTest();
 
         internal DebugMenu()
         {
             //create
-            imi = new IMenuItem[3];
+            imi = new IMenuItem[4];
 
             //populate
             imi[0] = unsorted.GetMenu();
             imi[1] = spectrum.GetMenu();
-            imi[2] = new HardExitGameState();
+            imi[2] = tatltael.GetMenu();
+            imi[3] = new HardExitGameState();
 
             //insert into a MenuState
             Menu debugMenu = new Menu("Debugger Menu",imi);
