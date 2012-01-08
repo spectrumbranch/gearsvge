6bf154218a3a73effd81dec341d337253227037b
unknown <Joseph@.(none)>
2012-01-07 22:57:40 -0500

    added linear movement for enemy unit

---
+++ GearsDebug/GearsDebug/Playable/RadialAssault/JoeState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Gears.Navigation;
+
+namespace GearsDebug.Playable.RadialAssault
+{
+    class JoeState : MenuReadyGameState
+    {
+        public JoeZone joeZone;
+
+        public JoeState()
+        {
+            MenuText = "Enemy Unit 1";
+            Initialize();
+        }
+        private void Initialize()
+        {
+            joeZone = new JoeZone();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            joeZone.Update(gameTime);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            joeZone.Draw(spriteBatch);
+        }
+    }
+}
