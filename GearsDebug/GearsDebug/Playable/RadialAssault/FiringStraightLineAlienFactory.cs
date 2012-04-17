2cc9a630f30d35bc4de817e5b537211071898143
spectrum <spectrum@spectrumbranch.com>
2012-04-16 22:26:20 -0400

    Pulled a Steve/Matt and forgot to git add files.

---
+++ GearsDebug/GearsDebug/Playable/RadialAssault/FiringStraightLineAlienFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using Gears.Cloud;
+using Gears.Navigation;
+using Gears.Playable;
+
+namespace GearsDebug.Playable.RadialAssault
+{
+    // Change to Alien and give it a specific type
+    sealed internal class FiringStraightLineAlienFactory : UnitTypeFactory
+    {
+        //TEMPORARY
+        Vector2 WORMHOLE_COORDINATES = new Vector2(ViewportHandler.GetWidth()/2, ViewportHandler.GetHeight()/2);
+        Vector2 PLAYER_STARTING_LOCATION;//move to playermanager
+        Vector2 PLAYER_IMAGE_ORIGIN = new Vector2(32,32);//hardcoded, bad chris
+        
+        private EnemyShip[] es;
+
+        internal FiringStraightLineAlienFactory()
+        {
+            //PLAYER_STARTING_LOCATION = new Vector2(WORMHOLE_COORDINATES.X, 1); //hardcode magic
+            PLAYER_STARTING_LOCATION = new Vector2(WORMHOLE_COORDINATES.X, WORMHOLE_COORDINATES.Y + (196 + PLAYER_IMAGE_ORIGIN.Y)); //hardcode magic
+            Register();
+        }
+        private void Register()
+        {
+            //es = new EnemyShip[1];      //hardcode magic
+
+            // es[0] = new EnemyShip(PLAYER_STARTING_LOCATION, Color.Azure, 0.0f, PLAYER_IMAGE_ORIGIN);    //TODO: fix up constructor.
+                                        //note that this constructor is default for testing only. 
+                                        //each unit will DEFINITELY have a different constructor.
+
+            //base.Register(es);
+        }
+    }
+}
---
+++ GearsDebug/GearsDebug/Playable/RadialAssault/SineAlienFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using Gears.Cloud;
+using Gears.Navigation;
+using Gears.Playable;
+
+namespace GearsDebug.Playable.RadialAssault
+{
+    // Change to Alien and give it a specific type
+    sealed internal class SineAlienFactory : UnitTypeFactory
+    {
+        //TEMPORARY
+        private Vector2 WORMHOLE_COORDINATES = new Vector2(ViewportHandler.GetWidth() / 2, ViewportHandler.GetHeight() / 2);
+        private Vector2 PLAYER_STARTING_LOCATION;//move to playermanager
+        private Vector2 PLAYER_IMAGE_ORIGIN = new Vector2(32, 32);//hardcoded, bad chris
+        private Vector2 originOfCircle = new Vector2(ViewportHandler.GetWidth() / 2, ViewportHandler.GetHeight() / 2);
+        
+        private SineAlien[] enemies;
+
+        internal SineAlienFactory()
+        {
+            //PLAYER_STARTING_LOCATION = new Vector2(WORMHOLE_COORDINATES.X, 1); //hardcode magic
+            PLAYER_STARTING_LOCATION = new Vector2(WORMHOLE_COORDINATES.X, WORMHOLE_COORDINATES.Y + (196 + PLAYER_IMAGE_ORIGIN.Y)); //hardcode magic
+            Register();
+        }
+        private void Register()
+        {
+            enemies = new SineAlien[1];      //hardcode magic
+
+            enemies[0] = new SineAlien(originOfCircle, Color.Red, 0.0f, "test");    //note that this constructor is default for testing only. 
+            //each unit will DEFINITELY have a different constructor.
+
+            base.Register(enemies);
+        }
+    }
+}
