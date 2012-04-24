3ac2319cef5d868543ad27b541e789b1aa46d418
unknown <Matt@Matt-PC.(none)>
2012-04-23 23:17:49 -0400

    redits!

--- GearsDebug/GearsDebug/Playable/DevTestArea/Matt Test/MattMenu.cs
+++ GearsDebug/GearsDebug/Playable/DevTestArea/Matt Test/MattMenu.cs
@@ -3,11 +3,12 @@ using GearsDebug.Cartography;
 using GearsDebug.Playable.RadialAssault;
 using GearsDebug.Playable.Matt;
 
+
 namespace GearsDebug.Navigation
 {
     internal sealed class MattMenu
     {
-        internal IMenuItem[] sub = new IMenuItem[3];
+        internal IMenuItem[] sub = new IMenuItem[4];
 
         internal MattMenu()
         {
@@ -18,6 +19,7 @@ namespace GearsDebug.Navigation
             sub[0] = new MattState();
             sub[1] = new BackMenuOption();
             sub[2] = new HardExitGameState();
+            sub[3] = new MattCreditState();
         }
         internal Menu GetMenu()
         {
