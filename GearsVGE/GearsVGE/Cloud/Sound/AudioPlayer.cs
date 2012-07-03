f1fdbee171eb0d82ae7112a9da9b5ca1e97e40de
MGZero <steven.calandra@gmail.com>
2012-07-03 19:03:00 -0400

    Added sound files.  Need to figure out where the program exits so I can shut down the audio thread.
    Signed-off-by: MGZero <steven.calandra@gmail.com>

---
+++ GearsVGE/GearsVGE/Cloud/Sound/AudioPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Collections.Concurrent;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Gears.Cloud.Sound
+{
+    static class AudioPlayer
+    {
+        
+        private static ThreadStart _threadStarter = new ThreadStart(_playAudio);
+        private static BlockingCollection<sound> _audioData = new BlockingCollection<sound>();
+        private static Thread _audioThread = new Thread(_threadStarter);
+
+        public static void queueAudio(sound data)
+        {
+            _audioData.Add(data);
+        }
+
+        public static void start()
+        {
+            _audioThread.Start();
+        }
+
+        private static void _playAudio()
+        {
+            while (true)
+            {
+                sound temp = _audioData.Take();
+
+                if (temp.GetType() == typeof(SoundEffect))
+                {
+                    ((SoundEffect)temp.soundData).Play();
+                }
+                else if (temp.GetType() == typeof(Song))
+                {
+                    MediaPlayer.Play((Song)temp.soundData);
+                }
+                else
+                {
+                    throw new FormatException("The audio data passed was not recognized as a Song or SoundEffect.");
+                }
+            }
+        }
+    }
+}
---
+++ GearsVGE/GearsVGE/Cloud/Sound/sound.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace Gears.Cloud.Sound
+{
+    class sound
+    {
+        private object _data; //i'd rather this be generic, but it was too complicated
+        private Type _type;
+        public bool loop = false;
+
+        public sound(SoundEffect data)
+        {
+            _data = data;
+            _type = typeof(SoundEffect);
+        }
+
+        public sound(Song data, bool loop = false)
+        {
+            _data = data;
+            _type = typeof(Song);
+            this.loop = loop;
+        }
+
+        public object soundData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public Type songOrEffect
+        {
+            get
+            {
+                return _type;
+            }
+        }
+
+    }
+}
