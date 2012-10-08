471af94ffa0b30cc668b4c32d336ae928dd67383
Christopher Bebry <spectrum@spectrumbranch.com>
2012-10-08 19:40:41 -0400

    Added InputHandler support for Mouse and GamePad.

--- GearsVGE/GearsVGE/Cloud/Input/InputHandler.cs
+++ GearsVGE/GearsVGE/Cloud/Input/InputHandler.cs
@@ -10,11 +10,21 @@ namespace Gears.Cloud.Input
     public abstract class InputHandler
     {
         public delegate void KeyboardStateEvent(ref KeyboardState CURRENT_KEYBOARD_STATE, ref KeyboardState OLD_KEYBOARD_STATE);
-        protected event KeyboardStateEvent keyboardEventList;
+        public delegate void MouseStateEvent(ref MouseState CURRENT_MOUSE_STATE, ref MouseState OLD_MOUSE_STATE);
+        public delegate void GamePadStateEvent(ref GamePadState CURRENT_GAMEPAD_STATE, ref GamePadState OLD_GAMEPAD_STATE);
 
+        protected event KeyboardStateEvent keyboardEventList;
+        protected event MouseStateEvent mouseEventList;
+        protected event GamePadStateEvent gamePadEventList;
 
         protected KeyboardState _oldKeyboardState;
         protected KeyboardState _currentKeyboardState;
+
+        protected MouseState _oldMouseState;
+        protected MouseState _currentMouseState;
+
+        protected GamePadState _oldGamePadState;
+        protected GamePadState _currentGamePadState;
 
         public virtual void Update(GameTime gameTime)
         {
@@ -22,20 +32,33 @@ namespace Gears.Cloud.Input
             {
                 keyboardEventList(ref _currentKeyboardState, ref _oldKeyboardState);
             }
+            if (mouseEventList != null)
+            {
+                mouseEventList(ref _currentMouseState, ref _oldMouseState);
+            }
+            if (gamePadEventList != null)
+            {
+                gamePadEventList(ref _currentGamePadState, ref _oldGamePadState);
+            }
         }
-
         public void ClearEventHandler()
         {
             keyboardEventList = null;
+            mouseEventList = null;
+            gamePadEventList = null;
         }
-
-
         public void SubscribeInputHook(KeyboardStateEvent kse)
         {
             keyboardEventList += kse;
         }
+        public void SubscribeInputHook(MouseStateEvent mse)
+        {
+            mouseEventList += mse;
+        }
+        public void SubscribeInputHook(GamePadStateEvent gpse)
+        {
+            gamePadEventList += gpse;
+        }
 
     }
-
-
 }
